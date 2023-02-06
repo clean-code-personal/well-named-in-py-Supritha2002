26b597eafb283d5650b00c3a535d453d7b7aa02a
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-02-06 18:06:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,18 +6,14 @@ MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
 
-
-def test_number_to_pair(pair_number,
-                        expected_major_color, expected_minor_color):
+def test_number_to_pair(pair_number,expected_major_color, expected_minor_color):
   major_color, minor_color = get_color_from_pair_number(pair_number)
   assert(major_color == expected_major_color)
   assert(minor_color == expected_minor_color)
 
-
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
-
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
@@ -26,5 +22,3 @@ if __name__ == '__main__':
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
-
-
